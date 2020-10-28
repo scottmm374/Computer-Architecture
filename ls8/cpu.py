a3ab35a52c5111ded3468174b34e4cd1ac3d9398
Michelle Scott <scottmm374@gmail.com>
2020-10-28 19:56:01 -0400

    added notes, and started functions in cpu, setup

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,18 @@
 
 import sys
 
+"""
+
+Step 1: Add the constructor to cpu.py
+Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers.
+
+Hint: you can make a list of a certain number of zeros with this syntax:
+
+x = [0] * 25  # x is a list of 25 zeroes
+
+"""
+
+
 class CPU:
     """Main CPU class."""
 
@@ -18,25 +30,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +59,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -63,3 +74,37 @@ class CPU:
     def run(self):
         """Run the CPU."""
         pass
+
+    def ram_read(self):
+
+        # should accept the address to read and return the value stored there.
+
+        pass
+
+    def ram_write(self):
+        # should accept a value to write, and the address to write it to.
+        pass
+
+    def HLT(self):
+        '''
+        Implement HLT instruction handler, similar to exit() in python
+        In run() in your if-else block, exit the loop if a HLT instruction is encountered, regardless of whether or not there are more lines of code in the LS-8 program you loaded.
+        '''
+        pass
+
+    def LDI(self):
+        '''
+        This instruction sets a specified register to a specified value.
+        See the LS-8 spec for the details of what this instructions does 
+        and its opcode value.
+        '''
+        pass
+
+    def PRN(self):
+        '''
+        This is a very similar process to adding LDI, but the handler is simpler. See the LS-8 spec.
+        At this point, you should be able to run the program and have it print 8 to the console!
+        '''
+        pass
+
+    # Left off at step 7 in readme
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -8,4 +8,18 @@ from cpu import *
 cpu = CPU()
 
 cpu.load()
+
+"""
+run()
+ workhorse of the processor. 
+reads memory address stored in register PC
+then store result in IR (instruction register)
+Can be local variable in run()
+Some instructions requires up to the next two bytes of data after the PC in memory to perform operations on. Sometimes the byte value is a register number, other times it's a constant value (in the case of LDI). Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b in case the instruction needs them.
+
+Then, depending on the value of the opcode, perform the actions needed for the instruction per the LS-8 spec. Maybe an if-elif cascade...? There are other options, too.
+
+After running code for any particular instruction, the PC needs to be updated to point to the next instruction for the next iteration of the loop in run(). The number of bytes an instruction uses can be determined from the two high bits (bits 6-7) of the instruction opcode. See the LS-8 spec for details.
+"""
+
 cpu.run()
