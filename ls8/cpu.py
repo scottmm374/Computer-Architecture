7676b3edbee349df573beeeb0ea89513f504f6f0
Michelle Scott <scottmm374@gmail.com>
2020-10-30 14:57:54 -0400

    printing 8, hardcoded

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,11 @@
 """CPU functionality."""
 
 import sys
+# Codes
+
+HLT = 0b00000001
+PRN = 0b01000111
+LDI = 0b10000010
 
 """
 
@@ -19,7 +24,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.running = False
 
     def load(self):
         """Load a program into memory."""
@@ -73,37 +81,85 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # set running to True
+        self.running = True
+        while self.running:
+            #  read memory address  stored in register pc
+            ir = self.ram_read(self.pc)
+            self.ram_read(self.pc)
+            # store values from ram_read into variables in case they are needed.
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+            self.trace()
+            if ir == LDI:
+                self.ldi(op_a, op_b)
 
-    def ram_read(self):
+            elif ir == PRN:
+                self.prn(op_a, op_b)
 
-        # should accept the address to read and return the value stored there.
+            elif ir == HLT:
+                self.hlt()
 
-        pass
+    def ram_read(self, mar):
+        return self.ram[mar]
 
-    def ram_write(self):
-        # should accept a value to write, and the address to write it to.
-        pass
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
-    def HLT(self):
+    def hlt(self):
+
+        self.running = False
+        self.pc += 1
         '''
         Implement HLT instruction handler, similar to exit() in python
         In run() in your if-else block, exit the loop if a HLT instruction is encountered, regardless of whether or not there are more lines of code in the LS-8 program you loaded.
+
+        `HLT`
+        Halt the CPU (and exit the emulator).
+
+        Machine code:
+
+        00000001 
+        01
+
         '''
         pass
 
-    def LDI(self):
+    def ldi(self, *operands):
+        self.reg[operands[0]] = operands[1]
+        self.pc += 3
+
         '''
         This instruction sets a specified register to a specified value.
         See the LS-8 spec for the details of what this instructions does 
         and its opcode value.
+
+        Set the value of a register to an integer.
+        Machine code:
+
+        10000010 00000rrr iiiiiiii
+        82 0r ii
+
         '''
         pass
 
-    def PRN(self):
+    def prn(self, *operands):
+        print(self.reg[operands[0]])
+        self.pc += 2
         '''
         This is a very similar process to adding LDI, but the handler is simpler. See the LS-8 spec.
         At this point, you should be able to run the program and have it print 8 to the console!
+
+        Print numeric value stored in the given register.
+
+        Print to the console the decimal integer value that is stored in the given
+        register.
+
+        Machine code:
+
+        01000111 00000rrr
+        47 0r
+
         '''
         pass
 
